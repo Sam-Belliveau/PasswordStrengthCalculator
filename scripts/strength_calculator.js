1e820ce288bc070f7f69e1f260e64aa2070bee10
SamBelliveau <sam.belliveau@gmail.com>
2020-08-19 16:02:45 -0400

    added entropy calculation

--- scripts/strength_calculator.js
+++ scripts/strength_calculator.js
@@ -1,14 +1,49 @@
 
-function getLetterStrength(letter) {
+function getLetterBits(letter) {
     let prob = PASSWORD_LET_PROBS[letter]
-    let strength = Math.log(1.0 / prob) / Math.log(2.0)
+    let strength = Math.log2(1.0 / prob)
     return strength
 }
 
-function getPasswordStrength(password) {
-    let total = 0.0;
+function getPasswordRawBits(password) {
+    let total = 0.0
+    
     for(let letter of password) {
-        total += getLetterStrength(letter);
+        total += getLetterStrength(letter)
     }
-    return total.toFixed(3)
+    
+    return total
+}
+
+function getPasswordEntropy(password) {
+    let length = 0
+    let freqCount = {}
+
+
+    for(let letter of password) {
+        length += 1
+        if(letter in freqCount) {
+            freqCount[letter] += 1
+        } else {
+            freqCount[letter] = 1
+        }
+    }
+
+    entropy = 0
+    for(let letter in freqCount) {
+        freq = freqCount[letter] / length
+        entropy += freq * Math.log2(freq)
+    }
+
+    return entropy
+}
+
+function getPasswordBits(password) {
+    let total = 0.0
+    
+    for(let letter of password) {
+        total += getLetterStrength(letter)
+    }
+    
+    return total * getPasswordEntropy(password)
 }
